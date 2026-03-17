07bc2c73bd9c857e9491b2430b5941eee004396e
Balkrishna Jha <your-github-bkrishnajha494@gmail.com>
2026-03-17 19:20:57 +0530

    deletion

--- BinarySearchTree/intro.cpp
+++ BinarySearchTree/intro.cpp
@@ -120,11 +120,67 @@ Node* maxVal(Node* root){
     }
     return temp;
 }
+Node* deleteFromBST(Node* root,int val){
+    //base case
+    if(root==NULL){
+        return root;
+    }
+    if(root->data==val){
+        //0 child
+        if(root->left==NULL && root->right==NULL){
+            delete root;
+            return NULL;
+        }
+        //1 child 
+        //left child
+        if(root->left!=NULL && root->right==NULL){
+            Node* temp=root->left;
+            delete root;
+            return temp;
+        }
+        //right child
+        if(root->left==NULL && root->right!=NULL){
+            Node* temp=root->right;
+            delete root;
+            return temp;
+        }
+        //2 child
+        if(root->left!=NULL && root->right!=NULL){
+            int mini=minVal(root->right)->data;
+            root->data=mini; 
+            root->right=deleteFromBST(root->right,mini);
+            return root;
+        }
+
+    }
+    else if(root->data>val){
+        //left part me chale jao
+        root->left=deleteFromBST(root->left,val);
+        return root;
+    }
+    else{
+        //right part me chale jao
+        root->right=deleteFromBST(root->right,val);
+        return root;
+    }
+}
 int main(){
     Node* root=NULL;
     cout<<"Enter data to create BSt"<<endl;
     takeInput(root);
+    
     cout<<"printing the BST"<<endl;
+    levelOrderTraversal(root);
+    cout<<"printing the BST"<<endl;
+    inorder(root);
+    cout<<"printing the BST"<<endl;
+    preorder(root);
+    cout<<"printing the BST"<<endl;
+    postorder(root);
+    cout<<"min val is"<<minVal(root)->data<<endl;
+    cout<<"max val is "<<maxVal(root)->data<<endl;
+    root=deleteFromBST(root,30);
+       cout<<"printing the BST"<<endl;
     levelOrderTraversal(root);
     cout<<"printing the BST"<<endl;
     inorder(root);
---
+++ BinarySearchTree/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+root=deleteFromBST(root,30);
