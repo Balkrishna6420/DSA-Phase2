42193e3eb0f2aed3c0f1ae638131ac7dd1d1bf32
Balkrishna Jha <your-github-bkrishnajha494@gmail.com>
2026-04-04 12:12:10 +0530

    Heapsort

--- Heaps/intro.cpp
+++ Heaps/intro.cpp
@@ -58,6 +58,32 @@ class heap{
     }
 
 };
+void heapify(int arr[],int n,int i){
+    int largest=i;
+    int left=2*i;
+    int right=2*i+1;
+
+    if(left <= n && arr[largest]<arr[left]){
+        largest=left;
+    }
+    if(right <=n && arr[largest]<arr[right]){
+        largest=right;
+    }
+    if(largest!=i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
+    }
+}
+void heapSort(int arr[],int n){
+    int size=n;
+    while(size>1){
+        //step1
+        swap(arr[size],arr[1]);
+        size--;
+        //step 2
+        heapify(arr,size,1);
+    }
+}
 int main(){
     heap h;
     h.insert(50);
@@ -68,9 +94,60 @@ int main(){
     h.print();
     h.deletefromHeap();
     h.print();
+    int arr[6]={-1,54,53,55,52,50};
+    int n=5;
+    //heap creation
+    for(int i=n/2;i>0;i--){
+        heapify(arr,n,i);
+    }
+    cout<<"printing the array now"<<endl;
+    for(int i=1;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+
+    //heap sort
+    heapSort(arr,n);
+    cout<<"printing sorted array now"<<endl;
+    for(int i=1;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+    //printing priority queue here
+    //max heap
+    priority_queue<int>pq;
+    pq.push(4);
+    pq.push(2);
+    pq.push(5);
+    pq.push(3);
+    cout<<"element at top"<<pq.top()<<endl;
+    pq.pop();
+    cout<<"element at top"<<pq.top()<<endl;
+    cout<<"size "<<pq.size()<<endl;
+    if(pq.empty()){
+        cout<<"pq is empty"<<endl;
+    }else{
+        cout<<"pq is not empty"<<endl;
+    }
+    
+    //min heap
+    priority_queue<int,vector<int>,greater<int>>minheap;
+    minheap.push(4);
+    minheap.push(2);
+    minheap.push(5);
+    minheap.push(3);
+    cout<<"element at top"<<minheap.top()<<endl;
+    minheap.pop();
+    cout<<"element at top"<<minheap.top()<<endl;
+    cout<<"size "<<minheap.size()<<endl;
+    if(minheap.empty()){
+        cout<<"minheap is empty"<<endl;
+    }else{
+        cout<<"minheap is not empty"<<endl;
+    }
 
     return 0;
 
 }
-//tc in case of insertion and deletion is O(logn)
+//tc in case of insertion and deletion & heapify is O(logn)
+//tc of heap sort is nlogn
 //note that dletion happnes only of root node so here 55 will be deleted
+//we dont have to use max heap min heap code again and again for it there us a data structure names priority queue use that imporyt using queue
